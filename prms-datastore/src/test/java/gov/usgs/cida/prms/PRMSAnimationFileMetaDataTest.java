b6808db58a40c202269552ed82d778466dd24148
Jordan Walker <jiwalker@usgs.gov>
2014-02-21 17:36:40 -0600

    added post-processed file to test

--- prms-datastore/src/test/java/gov/usgs/cida/prms/PRMSAnimationFileMetaDataTest.java
+++ prms-datastore/src/test/java/gov/usgs/cida/prms/PRMSAnimationFileMetaDataTest.java
@@ -9,29 +9,21 @@ import org.junit.*;
  * @author tkunicki
  */
 public class PRMSAnimationFileMetaDataTest {
-
-    public PRMSAnimationFileMetaDataTest() {
-    }
-
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-	}
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
+    
+    @Test
+    public void testOldAnimationFile() throws Exception {
+        URL url = getClass().getClassLoader().getResource("pcm.a1fi.1980-2099.annual.animation.nhru");
+        File oldMetaData = new File(url.getPath() + ".xml");
+        if (oldMetaData.exists()) {
+            oldMetaData.delete();
+        }
+       PRMSAnimationFileMetaData result = PRMSAnimationFileMetaData.getMetaData(url);
+       System.out.println(result);
     }
     
     @Test
-    public void findAnimationFile() throws Exception {
-        URL url = getClass().getClassLoader().getResource("pcm.a1fi.1980-2099.annual.animation.nhru");
+    public void testNewAnimationFile() throws Exception {
+        URL url = getClass().getClassLoader().getResource("cccma_post-processed.nhru");
         File oldMetaData = new File(url.getPath() + ".xml");
         if (oldMetaData.exists()) {
             oldMetaData.delete();
